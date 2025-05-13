ee258aa4070d5242521a16790812c4bd8070636f
Oduamadi Ndubuisi <97983328+odujrn@users.noreply.github.com>
2025-05-13 11:17:23 +0300

    Update TestGreeter.java

---
+++ server/src/test/java/com/example/TestGreeter.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestGreeter {
+    @Test
+    public void testSayHello() {
+        Greeter greeter = new Greeter();
+        assertEquals("Hello, World!", greeter.sayHello());
+    }
+}
